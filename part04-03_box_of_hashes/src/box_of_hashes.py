02f16a7959cadbb11717f140c3a09509cd22f3e9
Uwabor, Collins <legendarycolins@yahoo.com>
2024-05-19 17:23:07 +0100

    Updated

--- part04-03_box_of_hashes/src/box_of_hashes.py
+++ part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,8 +1,23 @@
 # Copy here code of line function from previous exercise
+def line(integer, string):
 
+    if string == "":
+        print(integer * "*")
+        
+    else:
+     print(integer * string[0])
+
+    
+if __name__ == "__main__":
+    line(5, "LOL")
+        
+    
+    
 def box_of_hashes(height):
     # You should call function line here with proper parameters
-    line(10, "#")
+
+    for _ in range(height):
+            line(10, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
