7145c0a3b7e64670e10d635aaab7ea8de9905dfb
Uwabor, Collins <legendarycolins@yahoo.com>
2024-05-23 15:51:41 +0100

    updated

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -8,8 +8,8 @@ def line(integer, string):
      print(integer * string[0])
 def triangle(size):
     # You should call function line here with proper parameters
-    for i in range(size):
-     line(size, "#")
+    for i in range(1, size + 1):
+     line(i, "#")
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
