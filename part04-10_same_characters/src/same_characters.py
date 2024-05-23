64e54395782610bf3d3d5db899e8744627dbf437
Uwabor, Collins <legendarycolins@yahoo.com>
2024-05-23 19:03:45 +0100

    updated

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,12 @@
 # Write your solution here
+def same_chars(a,b,c):
+   a,b,c = str(a), int(b), int(c) 
+   if b < 0 or c < 0 or b >= len(a) or c >= len(a):
+        return False
+   else:
+      return  a[b] == a[c]
+       
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("coder", 1, 2))
--- part04-11_first_second_last/src/first_second_last.py
+++ part04-11_first_second_last/src/first_second_last.py
@@ -1,4 +1,16 @@
 # Write your solution here
+def first_word(param):
+     words = param.split()
+     return words[0] if len(words) > 0 else ""
+
+def second_word(param):
+     words = param.split()
+     return words[1] if len(words) > 1 else ""
+
+def last_word(param):
+     words = param.split()
+     return words[-1] if len(words) > 0 else ""
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     sentence = "once upon a time there was a programmer"
