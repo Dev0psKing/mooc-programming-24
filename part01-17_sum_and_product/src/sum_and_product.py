04678721d3ec43d70006f38cc0cc7bf952da24ac
Uwabor, Collins <legendarycolins@yahoo.com>
2024-05-25 14:32:09 +0100

    updated

--- part01-17_sum_and_product/src/sum_and_product.py
+++ part01-17_sum_and_product/src/sum_and_product.py
@@ -3,6 +3,7 @@ number1 = int(input("Nimber 1 "))
 number2 = int(input("Nimber 2 "))
 summation = number1 + number2
 product = number1 * number2
+
 print(f'The sum of the numbers: {summation}')
 print(f'The product of the numbers: {product}')
 
